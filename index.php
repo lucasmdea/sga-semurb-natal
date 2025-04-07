d471a60e31021fc0a47e4573427db12e86ffd199
Lucas Melo <46384786+lucasmdea@users.noreply.github.com>
2025-04-07 11:17:22 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Controle de Fichas SGA</title>
+
+    <!-- Bootstrap -->
+    <link href="novosga/public/css/bootstrap.min.css" rel="stylesheet">
+
+  </head>
+  <body>
+
+    <div class="container">
+      <div class="header clearfix">
+     
+        <h3 class="text-muted">SGA - Controle de Chamadas</h3>
+      </div>
+
+      <div class="jumbotron">
+	<p>Selecione uma opção de acesso</p>        
+	<p><a  class="btn btn-lg btn-success" href="/novosga/public" >Sistema de Controle de Chamadas</a></p>
+        <p><a  class="btn btn-lg btn-info" href="/novosga/painel" >Painel de Atendimento</a></p>
+      </div>
+
+    </div> <!-- /container -->
+
+  </body>
+</html>
